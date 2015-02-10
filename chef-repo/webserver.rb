96ac1b9346960214cf5ad920baf0666e15904d65
Brian Choate <brian.choate@pitch.ly>
2015-02-10 23:06:08 +0000

    Added webserver.rb

---
+++ chef-repo/webserver.rb
@@ -0,0 +1,14 @@
+package 'apache2'
+
+service 'apache2' do
+    action [:start, :enable]
+end
+
+
+file '/var/www/html/index.html' do
+    content '<html>
+    <body>
+        <h1>hello bitches</h1>
+    </body>
+</html>'
+end
